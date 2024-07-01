fcb1b7a6cc46724ece68c78eec3eea6b4e302a30
skolton09 <skolton09@gmail.com>
2024-07-01 03:51:07 -0700

    DONE

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,30 @@
-import Image from "next/image";
+'use client'
+
 import "./style.css";
+import { motion }  from 'framer-motion';
 
 export default function Home() {
   return (
-    <main>
-      <div class="catpage">
-        <p>Cats CATS CaTS MEOW</p>
-      </div>  
+    <main className="uh">
+      <div id = "right-side" className="side catpage">
+        <motion.div initial={{opacity: 0}} whileInView={{ opacity:[0, 0, 0, 0, 0.2, 0.2, 0.4, 0.4, 0.6, 0.8, 1]}} viewport={{once :true}}>
+        <motion.div animate={{y: 100}}>
+        <div className="title"> CATS!</div>
+        </motion.div>
+        <motion.div animate={{y: 1000}}>
+        <div className="title"> Meow</div>
+        </motion.div>
+        <motion.div animate={{y: 2000}}>
+        <div className="title"> Nya~</div>
+        </motion.div>
+        <motion.div animate={{y: 3000}}>
+        <div className="title">I Love you</div>
+        </motion.div>
+        <motion.div animate={{y: 3500}}>
+        <div className="title"> Happy Birthday! 🎂</div>
+        </motion.div>
+        </motion.div>
+      </div> 
     </main>
   );
 }
